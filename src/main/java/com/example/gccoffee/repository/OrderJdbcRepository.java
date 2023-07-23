a1f9398be45c962bdadb359878311c5870229c2e
sujjangOvO <pnk7038@gmail.com>
2023-07-23 23:11:00 +0900

    관리자 기능 OrderRepository 추가

--- src/main/java/com/example/gccoffee/repository/OrderJdbcRepository.java
+++ src/main/java/com/example/gccoffee/repository/OrderJdbcRepository.java
@@ -1,16 +1,22 @@
 package com.example.gccoffee.repository;
 
+import com.example.gccoffee.model.Email;
 import com.example.gccoffee.model.Order;
 import com.example.gccoffee.model.OrderItem;
+import com.example.gccoffee.model.OrderStatus;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
+import static com.example.gccoffee.JdbcUtils.toLocalDateTime;
+import static com.example.gccoffee.JdbcUtils.toUUID;
 
 @Component
 public class OrderJdbcRepository implements OrderRepository {
@@ -34,6 +40,22 @@ public class OrderJdbcRepository implements OrderRepository {
           toOrderItemParamMap(order.getOrderId(), order.getCreatedAt(), order.getUpdatedAt(), item)));
     return order;
   }
+
+  @Override
+  public List<Order> findAll() {
+    return jdbcTemplate.query("select * from orders", orderRowMapper);
+  }
+
+  private static final RowMapper<Order> orderRowMapper = (resultSet, i) -> {
+    var orderId = toUUID(resultSet.getBytes("order_id"));
+    var email = new Email(resultSet.getString("email"));
+    var address = resultSet.getString("address");
+    var postcode = resultSet.getString("postcode");
+    var orderStatus = OrderStatus.valueOf(resultSet.getString("order_status"));
+    var createdAt = toLocalDateTime(resultSet.getTimestamp("created_at"));
+    var updatedAt = toLocalDateTime(resultSet.getTimestamp("updated_at"));
+    return new Order(orderId, email, address, postcode,orderStatus, createdAt, updatedAt);
+  };
 
   private Map<String, Object> toOrderParamMap(Order order) {
     var paramMap = new HashMap<String, Object>();
--- src/main/java/com/example/gccoffee/repository/OrderRepository.java
+++ src/main/java/com/example/gccoffee/repository/OrderRepository.java
@@ -3,9 +3,12 @@ package com.example.gccoffee.repository;
 import com.example.gccoffee.model.Order;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository {
 
   Order insert(Order order);
 
+  List<Order> findAll();
 }
